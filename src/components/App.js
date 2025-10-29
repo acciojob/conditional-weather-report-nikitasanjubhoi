0fa8495df9900bb06e01d957e9bcc1ea1d54e558
Nikita bhoi <114177037+nikitasanjubhoi@users.noreply.github.com>
2025-10-29 20:32:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [weather] = useState({
+    temperature: 25,
+    conditions: "Sunny"
+  });
+
+  const tempStyle = {
+    color: weather.temperature > 20 ? "red" : "blue"
+  };
   return (
     <div>
         {/* Do not remove the main div */}
+    <p> 
+    Temperature: <span style={tempStyle}>{weather.temperature}</span>
+  </p>
+  <p>Conditions: {weather.conditions}</p>
     </div>
   )
 }
